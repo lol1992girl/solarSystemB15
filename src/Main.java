755017aeae874b3af844921d101d7c8a097bf72d
Kate Frasynych <katefrasynych@kates-air.myfiosgateway.com>
2023-03-29 15:34:42 -0400

    i have successfully resulved the issue and code is ready to be merged in the sun branch

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         solar.sun="1";
         solar.moon="2";
         solar.planets="2";
+        featuresSun Xron=new featuresSun();
+        Xron.temperature="12345672242k";
+        Xron.radius="1312313133"";
+        Xron.color="pink";
     }
 }
